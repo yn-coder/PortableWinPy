33b8194619a22ea104cc2b9d79c368351397ab44
yn_coder <yn.coder@gmail.com>
2017-11-18 11:36:27 +0300

    #4

--- Python/Bottle/bottle_app.py
+++ Python/Bottle/bottle_app.py
@@ -9,9 +9,12 @@ def index():
 @error(404)
 def error404(error):
     return template( '404' )
-        
-import os
-# Change working directory so relative paths (and template lookup) work again
-os.chdir(os.path.dirname(__file__))
 
-application = default_app()
+import os
+
+if __name__ == "__main__":
+    run(host='localhost', port=8081, reloader=True, debug=True)
+else:
+    # Change working directory so relative paths (and template lookup) work again
+    os.chdir(os.path.dirname(__file__))
+    application = default_app()
--- Python/Bottle/index.tpl
+++ Python/Bottle/index.tpl
@@ -3,4 +3,5 @@
 <h1>Your Bottle WSGI application is up and running!</h1>
 <p>Current Python version is</p>
 % import sys
-{{ sys.version_info }}
+<strong>
+{{ sys.version_info }}</strong>
